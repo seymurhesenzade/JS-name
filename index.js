6bb8b963194d0c9d81cbf653f1ebe06537cedc45
seymurhesenzade <hasanzadeseymur21@gmail.com>
2024-04-24 10:43:16 +0400

    Input

---
+++ index.js
@@ -0,0 +1,38 @@
+const name = document.getElementById("#name");
+const surname = document.getElementById("#surname");
+const salary = document.getElementById("#salary");
+const age = document.getElementById("#age");
+const username = document.getElementById("#username");
+const password = document.getElementById("#password");
+const email = document.getElementById("#email");
+const nationality = document.getElementById("#nationality");
+const position = document.getElementById("#position");
+const experience = document.getElementById("#experience");
+const button = document.getElementById(".btn")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
